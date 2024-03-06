eff9de3d2176a68afacfeb1ebda4f70b51a4edfd
milosdjurica <milosdjurica13@gmail.com>
2024-03-06 14:23:01 +0100

    added tabs component from shadcn and created menu and about components

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h3>About goodies</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima cum
+        aliquid voluptatibus voluptas iusto! Deserunt, dolore nisi repudiandae
+        esse accusantium aliquam perspiciatis itaque dicta! Nesciunt quasi eos
+        odio provident debitis.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <div>
+      <h3>Menu</h3>
+      <div>
+        <div>Card</div>
+        <div>Card</div>
+        <div>Card</div>
+      </div>
+    </div>
+  );
+}
